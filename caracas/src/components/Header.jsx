e0a4d732836a18685da6e656968f66cf4dab1ac4
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-08-05 10:26:22 -0400

    fixed links

--- caracas/src/components/Header.jsx
+++ caracas/src/components/Header.jsx
@@ -8,14 +8,14 @@ function Header() {
         <div className="row">
           <div className="fix-scroll">
             <div className="one-half column">
-              <img className="logo" src={calogo} />
+              <a href="/"><img className="logo" src={calogo} /></a>
             </div>
             <div className="one-half column">
               <div className="header-links">
                 <ul className="mainNav">
                   <li><a href="/#menu-section">Menu</a></li>
                   <li><a href="/#location-section">Hours & Location</a></li>
-                  <li><a href="https://ordering.chownow.com/order/4265/locations"><button className="flynn-button">ORDER ONLINE</button></a></li>
+                  <li><a href="https://ordering.chownow.com/order/4265/locations" target="_blank"><button className="flynn-button">ORDER ONLINE</button></a></li>
                 </ul>
               </div>
             </div>
