97d6248db4a5baf11bebe680ec5fcdc9fa25caad
Riti Sharma <riti.sharma11@yahoo.com>
2019-08-05 10:20:12 -0400

    header

--- caracas/src/components/Header.jsx
+++ caracas/src/components/Header.jsx
@@ -6,15 +6,19 @@ function Header() {
     <>
       <div className="container is-fluid">
         <div className="row">
-          <div className="one-half column">
-            <img className="logo" src={calogo} />
-          </div>
-          <div className="one-half column">
-            <ul className="mainNav">
-              <li><a href="/#menu-section">Menu</a></li>
-              <li><a href="/#location-section">Hours & Location</a></li>
-              <li><a href="https://ordering.chownow.com/order/4265/locations"><button className="flynn-button">ORDER ONLINE</button></a></li>
-            </ul>
+          <div className="fix-scroll">
+            <div className="one-half column">
+              <img className="logo" src={calogo} />
+            </div>
+            <div className="one-half column">
+              <div className="header-links">
+                <ul className="mainNav">
+                  <li><a href="/#menu-section">Menu</a></li>
+                  <li><a href="/#location-section">Hours & Location</a></li>
+                  <li><a href="https://ordering.chownow.com/order/4265/locations"><button className="flynn-button">ORDER ONLINE</button></a></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
